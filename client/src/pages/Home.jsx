039d8ed8c60ddf843d71fbda1eeac6ff5b4b1bb1
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-02 12:54:42 +0530

    updated home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,9 +6,9 @@ import HeroSection from '../components/Custom/HeroSection'
 
 function Home() {
     return (
-        <div className="flex flex-col min-h-screen w-full bg-gray-50">
+        <div className="flex flex-col min-h-screen w-screen bg-gray-50 overflow-x-hidden">
             <Navbar />
-            <main className="flex flex-col flex-1 items-center justify-start w-full">
+            <main className="flex flex-col flex-1 items-center justify-start w-full h-full">
                 <h1 className="mt-10 text-3xl md:text-5xl font-extrabold text-green-900 text-center">Where to?</h1>
                 <CategoryFilters />
                 <SearchBar />
