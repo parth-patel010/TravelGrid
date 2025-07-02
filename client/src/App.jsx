673df86fbc727dc66548559143cc1065c8def3f8
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-02 12:10:45 +0530

    testing of ui addition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Hello welcome</h2>
+      <h2>Hello, Welcome To GSSOC 2025</h2>
+      <Button>Start Contributing</Button>
+
     </>
   )
 }
