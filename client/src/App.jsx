4aa0693d3a5ac16462a02c8d5e981a76a2bb57a5
Pranathi <pranuk2004@gmail.com>
2025-07-24 12:51:30 +0530

    Add 'Go to Top' button for enhanced user experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { AppProvider } from './context/AppContext'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Custom/Navbar'
-import Footer from './components/Custom/Footer'
-import { AuthProvider } from './context/AuthContext'
+import React from 'react';
+import { AppProvider } from './context/AppContext';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Custom/Navbar';
+import Footer from './components/Custom/Footer';
+import { AuthProvider } from './context/AuthContext';
+import GoToTopButton from './components/GoToTopButton'; 
 
 function App() {
   return (
@@ -14,11 +15,14 @@ function App() {
           <div className="flex-grow">
             <Outlet />
           </div>
+          {/* Place the GoToTopButton here, outside the flex-grow div
+              but within the main flex container, so it's always visible and fixed. */}
+          <GoToTopButton />
           <Footer />
         </div>
       </AppProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
