332200ddd6cd68a4a2e108fa4de9a7f37d7e2fe1
Prachi Thakor <thakorprachi08@gmail.com>
2025-07-29 22:36:19 +0530

    Add cross origin access for successful backend access

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+// Allow specific origin (frontend)
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 app.use(express.json());
 
 // Routes
