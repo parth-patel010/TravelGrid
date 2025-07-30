bad17d504d5aa1c687190bf170718c514716b77b
Prachi Thakor <thakorprachi08@gmail.com>
2025-07-30 12:50:55 +0530

    Add successfully deployed backend url

--- Server/index.js
+++ Server/index.js
@@ -16,16 +16,18 @@ connectDB();
 const allowedOrigins = ['http://localhost:5173', 'https://travel-grid.vercel.app'];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // if you're sending cookies or auth headers
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
-// app.options('*', cors()); // handle preflight
+
 
 app.use(express.json());
 
