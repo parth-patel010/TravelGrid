5296848477ccb78068bf84055a9dae4c9b654eb2
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2025-08-01 17:43:20 +0530

    fixed conflicts

--- client/src/components/GoToTopButton.jsx
+++ client/src/components/GoToTopButton.jsx
@@ -39,16 +39,11 @@ const GoToTopButton = () => {
   return (
     <div className="fixed bottom-6 left-6 z-40">
       <button
-        className={`w-14 h-14 bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700 rounded-full shadow-lg  transition-all duration-300 transform  flex items-center opacity-0 justify-center cursor-pointer
-        ${
-          showButton
-            ? "opacity-100 visible pointer-events-auto translate-y-0"
-            : ""
-        }
-        `}
+        className={`w-14 h-14 bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700 rounded-full shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110 flex items-center justify-center cursor-pointer ${
+          showButton ? "" : "opacity-0"
+        }`}
         onClick={scrollToTop}
         aria-label="Go to top of page"
-  
       >
         <ArrowUp className="stroke-white" />
       </button>
