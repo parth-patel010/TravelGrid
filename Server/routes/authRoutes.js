bcd10dd903f69ea69c06137ac3819eabd4f7bf62
Pragyan Lahkar <pragyanlahkar28@gmail.com>
2025-08-05 11:02:09 +0530

    feat: updated auth routes to support cookie-based login/logout and /me

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, registerUser, googleAuth } = require('../controller/authController');
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  googleAuth,
+  getCurrentUser, // /me route handler
+} = require('../controller/authController');
+const { verifyJWT } = require('../middleware/auth');
 
-router.post('/login', loginUser);
 router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/logout', logoutUser);
 router.post('/google', googleAuth);
+// @access  Private (protected route)
+router.get('/me', verifyJWT, getCurrentUser);
 
 module.exports = router;
