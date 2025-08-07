786176d012c2a24c98d06165e42eec2b11dcc0e5
Diksha Singh <dikshasingh25202520@gmail.com>
2025-08-07 19:06:23 +0530

    Fix: Updated TrendingSpots UI

--- client/src/pages/TrendingSpots.jsx
+++ client/src/pages/TrendingSpots.jsx
@@ -237,7 +237,7 @@ const TrendingSpots = () => {
   }
 
   return (
-    <div className="min-h-screen overflow-x-hidden transition-colors duration-300" style={{ background: 'var(--bg-primary)', color: 'var(--text-primary)',marginTop:"5rem" }}>
+    <div className={`min-h-screen overflow-x-hidden transition-colors  duration-300 ${isDarkMode ? "bg-gradient-to-br from-pink-400 black via-gray-900 to-pink-900" : ""}`} style={{ color: 'var(--text-primary)',marginTop:"5rem" }}>
 
       <Navbar lightBackground />
 
@@ -259,9 +259,9 @@ const TrendingSpots = () => {
         </div>
       </section>
 
+
       {/* Filter Tabs */}
-      {/* Filter Tabs */}
-      <div className="bg-pink-900 shadow-sm sticky top-0 z-10">
+      <div className={`bg-pink-900 shadow-sm  ${isDarkMode ? "" : ""}`} style={{marginTop:"-5rem", borderTop:"2px solid gray", position:"sticky" ,top:"0",zIndex:"10"}}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-start md:justify-center space-x-2 md:space-x-4 py-3 md:py-4 overflow-x-auto scrollbar-hide">
             {categories.map((category) => (
@@ -429,9 +429,8 @@ const TrendingSpots = () => {
           )}
         </div>
 
-        {/* Load More Button */}
-        {visibleCount < filteredSpots.length && (
-          <div className="text-center mt-8 md:mt-12">
+       
+          <div className="text-center mt-8 md:mt-12 mb-5">
             <button
               onClick={handleLoadMoreSpots}
               className="bg-gradient-to-r from-pink-600 to-pink-500 shadow-md hover:shadow-lg text-white px-6 md:px-8 py-2 md:py-3 rounded-lg font-semibold transition-all transform hover:scale-105 duration-200 cursor-pointer text-sm md:text-base"
@@ -439,7 +438,6 @@ const TrendingSpots = () => {
               Load More Trending Spots
             </button>
           </div>
-        )}
       </div>
     </div>
   );
