48ce53f2d6660949cc686f0235dcdc9a4a843940
ravneet kaur <146877258+ravneet27062004@users.noreply.github.com>
2025-08-08 15:13:09 +0530

    search

--- Server/index.js
+++ Server/index.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-
+const searchRoutes = require('./routes/searchRoutes'); 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // DB Connection
-connectDB();
+// connectDB();
 
 // Middleware
 app.use(cors());
@@ -20,7 +20,7 @@ app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'API is running smoothly!' });
 });
 app.use('/api/auth', authRoutes);
-
+app.use('/api/search', searchRoutes); 
 // server
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
